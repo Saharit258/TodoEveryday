9e356aacbda6da77e90a65ff17c6b99b5b87a90d
saharit258 <saharitme@gmail.com>
2024-03-18 10:16:11 +0700

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-      const newTodo = { id: Date.now(), text: text };
-      setTodos([...todos, newTodo]); 
-  };
-
-  const submit = () => {
-    const inputElement = document.querySelector('input[type="text"]');
-          addTodo(inputElement.value);
-          inputElement.value = ""; 
-  }
-
-  const remove = (id) => {
-    setTodos(todos.filter((todos) => todos.id !== id))
-  }
 
   return (
     <>
-      <div>
-        <input type="text" />
-        <button onClick={submit}>Submit</button>
-      </div>
-      <div>
-        {todos.map((todo) => (
-          <tr key={todo.id}>
-            <span>{todo.text}</span>
-            <button>Edit</button>
-            <button onClick={() => remove(todo.id)}>remove</button>
-          </tr>
-        ))}
-      </div>
+      <div></div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
